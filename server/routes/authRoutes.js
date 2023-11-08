c3890183de6649ca98d82a9b87a0ee480aaac3d3
wael ben elwafi <wael.belwafi@symdrik.com>
2023-11-08 23:42:41 +0100

    add rate-limiter in authentification

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,14 +8,22 @@ import {
   resetPassword,
 } from '../controllers/authController.js';
 import { authenticateUser } from '../middleware/authentication.js';
+import { rateLimit } from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', limiter, register);
+router.post('/login', limiter, login);
 router.delete('/logout', authenticateUser, logout);
 router.post('/verify-email', verifyEmail);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password', resetPassword);
+router.post('/forgot-password', limiter, forgotPassword);
+router.post('/reset-password', limiter, resetPassword);
 
 export default router;
